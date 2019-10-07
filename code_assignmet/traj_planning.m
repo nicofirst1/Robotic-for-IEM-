85bb274ac47f763e17d0747696e9da3da10cccc9
dizzi <nicofirst1@gmail.com>
2019-10-07 19:31:27 +0200

    reconfigure and split

--- code_assignmet/traj_planning.m
+++ code_assignmet/traj_planning.m
@@ -1,38 +1,68 @@
+clear;
+
 [robot,pArb]=initializer();
    
+%% Initial configuration
 % getting feasable cartesian position with forward kin
 % this is done in order to get a feaseble end point in the cartesian space
-Qf = [pi/3,pi/2,pi/4,pi/4,pi/6];
-Tf=pArb.fkine(Qf);
-Pf=Tf.t;
+
+Qi=[0,pi/2, 0, pi, pi/4];
+Qf=[0,-pi/2,-pi/4,0,pi/7];
+
+Pi= get_feasable_point(pArb,Qi);
+Pf = get_feasable_point(pArb,Qf);
+
+t=get_t(0,3,0.3);
+
+%% Joint Trajectory
+
+% get the values of configuration 
+[q,qd,qdd]=jtraj(Qi,Qf,t);
+% extract the coordinates in the end effector frame
+pe=pArb.fkine(q).tv;
+
+% plot 
+figure(1);
+plot_qs(q,qd,qdd,t,pe);
 
 
-% configuration at start
-Qi = [0, 0, 0, 0,0 ];
-% transformation at start/end
-Ti=pArb.fkine(Qi);
-Pi=Ti.t;
+figure(3);
+
+pcshow(pe);
+
+
+% plot robot movement
+figure(2);
+view(3);
+pArb.plot(q, 'loop');
+
+
+
+%% Cartesian Trajectory
+
+%cartesian_traj(Pi,Pf,pArb,t)
+
+
+%% Utils Functions
+
+function cartesian_traj(Pi,Pf,pArb,t)
+% Plan trajectory in the cartesian space,
+% Pi/Pf: start and end for the end effector cartesian position
+% pArb: serialLink
+% t: is the timing step
+
+
 % transformation at end
 Tf = SE3(Pf);
-
-% remove last joint from the inverse 
-m = [1 1 1 1 0 0];   
-Qf=pArb.ikine(Tf,Ti,'mask',m);
+Ti = SE3(Pi);
 
 
-% Getting the time 
-step=0.3;
-total_time=3;
-t =[0:step:total_time];
+Tc=ctraj(Ti,Tf,length(t))
+Pe = transl(Tc)
 
-% estimate trak
-[q,qd,qdd]=jtraj(Qi,Qf,t);
-
-%plot_qs(q,qd,qdd,t,pArb);
-
-
-
-function p=get_feasable_point(pArb,q)
+plot(t,Pe);
+title('End effecotr position');
+legend('x','y','z');
 
 
 
@@ -40,38 +70,68 @@ end
 
 
 
-function plot_qs(q,qd,qdd,t,pArb)
-
-view(3);
-pArb.plot(q);
 
 
-% Configuration plots
-figure(1);
+function t=get_t(start_t,end_t,step)
+
+    t =[start_t:step:end_t];
+end
+function p=get_feasable_point(pArb,q)
+% return a feasable cartesian point given the robot and a configuration
+% the configuration can be zero and then a random one in range [0, pi/2]
+% will be taken
+
+
+if q==0
+    qs=length(pArb.a);
+    q=(pi/2).*rand(qs,1) ;
+end
+
+p=pArb.fkine(q).t;
+
+end
+
+
+
+function plot_qs(q,qd,qdd,t,pe)
+
 
 % thetas position
 nexttile;
 plot(t,q);
-title('q');
+title('Joint position');
+legend('q1','q2','q3','q4','q5');
+xlabel("seconds");
+ylabel("rad");
 
 % thetas vel
 nexttile;
 plot(t,qd);
-title('qd');
+title('Joint velocity');
+legend('qd1','qd2','qd3','qd4','qd5');
+xlabel("seconds");
+ylabel("rad/s");
+
 
 % thetas accel
 nexttile;
 plot(t,qdd);
-title('qdd');
+title('Joint accleration');
+legend('qdd1','qdd2','qdd3','qdd4','qdd5');
+xlabel("seconds");
+ylabel("rad/s^2");
 
 
+% endeffector position 
 nexttile;
-tmp=plot(t,q);
-title('Legends');
+plot(t,pe);
+title('End effector coordinates');
+legend('x','y','z');
+xlabel("seconds");
+ylabel("coord");
 
-legHandle =legend('q1','q2','q3','q4','q5');
-set(tmp, 'visible', 'off');
-legHandle.TextColor = [0 0 0];
-legHandle.FontSize = 16;
+
+
+
 end
 
